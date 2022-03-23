a22d16c3323b5afcb86871ed776c84e3511a7cd4
Edy Segura <edysegura@gmail.com>
2022-03-22 22:01:59 -0300

    refactor: move <Square /> to its own file

--- src/index.js
+++ src/index.js
@@ -2,14 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { calculateWinner } from './calculateWinner';
 import './index.css';
-
-function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  )
-}
+import { Square } from './Square';
 
 class Board extends React.Component {
   renderSquare(i) {
