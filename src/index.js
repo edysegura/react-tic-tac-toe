970ab5fc8111f317482a9462fcaca7e6e9870c09
Edy Segura <edysegura@gmail.com>
2022-03-28 20:55:45 -0300

    refactor: move calculate winner function under helper folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Board } from './board/Board';
-import { calculateWinner } from './calculateWinner';
+import { calculateWinner } from './helper/calculateWinner';
 import './index.css';
 
 class Game extends React.Component {
